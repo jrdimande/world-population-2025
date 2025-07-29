03cc5c697f0dd8cd614f78595b9392d7683bf8a4
jrdimande <timoteojrdimande@gmail.com>
2025-07-29 20:44:43 +0200

    feat: add population distribution by continent pie chart

--- bar_chart.py
+++ bar_chart.py
@@ -40,7 +40,20 @@ y_axis_config = {'title' : 'Total Population'}
 # Set up the layout
 layout = Layout(title={'text': 'Top 10 most populated in the world', 'x' : 0.5},
                 xaxis=x_axis_config,
-                yaxis=y_axis_config)
+                yaxis=y_axis_config,
+                annotations=[
+                    dict(
+                        x=0, y=-0.15,
+                        showarrow=False,
+                        text="Fonte dos dados: <a href='https://worldpopulationreview.com'>https://worldpopulationreview.com</a>",
+                        xref='paper',
+                        yref='paper',
+                        font=dict(size=10, color='blue'),
+                        align='left'
+                    )
+                ]
+
+                )
 
 # Generate and open bar chart
-offline.plot({'data': data, 'layout' : layout}, filename='bar_chart.html')
+offline.plot({'data': data, 'layout' : layout}, filename='charts/bar_chart.html')
